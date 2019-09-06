76bd24aa178e767a1781ce7a70f77ca4db4b6e2a
julien-la-fabrik <j.canourgues@la-fabrik.nc>
2019-09-06 13:43:46 +1100

    carousel select

--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -1,7 +1,9 @@
 import Carousel from './Carousel'
 import SliderHero from './SliderHero'
 import Spotlight from './Spotlight'
+import Slider from './Slider'
+
 
 export default {
-  Carousel, SliderHero, Spotlight
+  Carousel, SliderHero, Spotlight, Slider
 }
