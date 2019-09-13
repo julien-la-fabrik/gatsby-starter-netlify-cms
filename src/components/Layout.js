f809560fb9df9a500ceb10161631ac3ff8bcc447
julien-la-fabrik <j.canourgues@la-fabrik.nc>
2019-09-13 14:39:32 +1100

    reindent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { StaticQuery, graphql } from 'gatsby'
 import Navbar from '../components/Navbar'
 import Offcanvas from '../components/Offcanvas'
 import Footer from '../components/Footer'
-
+import settings from '../settings';
 import './uikit.scss';
 
 class TemplateWrapper extends Component {
@@ -21,8 +21,8 @@ class TemplateWrapper extends Component {
   render() {
     return (
       <div>
-          <StaticQuery
-            query={graphql`
+        <StaticQuery
+          query={graphql`
               query HeadingQuery {
                 site {
                   siteMetadata {
@@ -32,54 +32,54 @@ class TemplateWrapper extends Component {
                 }
               }
             `}
-            render={data => (
-              <div>
-                <Helmet>
-                  <html lang="en" />
-                  <title>{data.site.siteMetadata.title}</title>
-                  <meta
-                    name="description"
-                    content={data.site.siteMetadata.description}
-                  />
+          render={data => (
+            <div>
+              <Helmet>
+                <html lang="en" />
+                <title>{settings.siteinfos.title}</title>
+                <meta
+                  name="description"
+                  content={settings.siteinfos.description}
+                />
 
-                  <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/img/apple-touch-icon.png"
-                  />
-                  <link
-                    rel="icon"
-                    type="image/png"
-                    href="/img/favicon-32x32.png"
-                    sizes="32x32"
-                  />
-                  <link
-                    rel="icon"
-                    type="image/png"
-                    href="/img/favicon-16x16.png"
-                    sizes="16x16"
-                  />
+                <link
+                  rel="apple-touch-icon"
+                  sizes="180x180"
+                  href="/img/apple-touch-icon.png"
+                />
+                <link
+                  rel="icon"
+                  type="image/png"
+                  href="/img/favicon-32x32.png"
+                  sizes="32x32"
+                />
+                <link
+                  rel="icon"
+                  type="image/png"
+                  href="/img/favicon-16x16.png"
+                  sizes="16x16"
+                />
 
-                  <link
-                    rel="mask-icon"
-                    href="/img/safari-pinned-tab.svg"
-                    color="#ff4400"
-                  />
-                  <meta name="theme-color" content="#fff" />
+                <link
+                  rel="mask-icon"
+                  href="/img/safari-pinned-tab.svg"
+                  color={settings.siteinfos.brandcolor}
+                />
+                <meta name="theme-color" content={settings.siteinfos.brandcolor} />
 
-                  <meta property="og:type" content="business.business" />
-                  <meta property="og:title" content={data.site.siteMetadata.title} />
-                  <meta property="og:url" content="/" />
-                  <meta property="og:image" content="/img/og-image.jpg" />
-                </Helmet>
-                <Navbar currentPath={this.state.currentPath}/>
-                <div>{this.props.children}</div>
-<Footer />
-                <Offcanvas currentPath={this.state.currentPath} />
+                <meta property="og:type" content="business.business" />
+                <meta property="og:title" content={settings.siteinfos.title} />
+                <meta property="og:url" content="/" />
+                <meta property="og:image" content="/img/og-image.jpg" />
+              </Helmet>
+              <Navbar currentPath={this.state.currentPath} />
+              <div>{this.props.children}</div>
+              <Footer />
+              <Offcanvas currentPath={this.state.currentPath} />
 
-              </div>
-            )}
-          />
+            </div>
+          )}
+        />
       </div>
     );
   }
